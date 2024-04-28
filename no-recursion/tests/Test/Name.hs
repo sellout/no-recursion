3bfbd7646d8bcf95c20f568e4b85f323e56fc5a6
Greg Pfeil <greg@technomadic.org>
2024-04-28 01:12:58 -0400

    Warn on recursion in methods
    It’s not currently possible to allow recursion in methods using `ANN`, so this
    makes it a warning instead of a failure (although some were previously ignored).

--- no-recursion/tests/Test/Name.hs
+++ no-recursion/tests/Test/Name.hs
@@ -6,6 +6,9 @@ module Test.Name
   )
 where
 
+-- NB: These unqualified modules come from semigroups in GHC <8, and base
+--     otherwise.
+import safe Data.Semigroup (Semigroup ((<>)))
 import safe "base" Control.Category (Category (id))
 
 recDef :: a -> b
@@ -14,3 +17,9 @@ recDef = recDef
 
 nonRecDef :: a -> a
 nonRecDef = id
+
+data Dummy = Dummy
+
+-- | This should result in warnings, but not failures.
+instance Semigroup Dummy where
+  Dummy <> Dummy = Dummy
