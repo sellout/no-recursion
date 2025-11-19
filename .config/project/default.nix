fb4b26594cf398656c27fa845f5b294e4080702d
Greg Pfeil <greg@technomadic.org>
2025-11-19 00:23:55 -0700

    Sync with the latest Flaky template
    This inherits more from the Flaky config and has the new `cabal` check.

--- .config/project/default.nix
+++ .config/project/default.nix
@@ -1,48 +1,30 @@
+### All available options for this file are listed in
+### https://sellout.github.io/project-manager/options.xhtml
 {
   config,
-  flaky,
   lib,
-  pkgs,
   self,
-  supportedSystems,
   ...
 }: {
   project = {
     name = "no-recursion";
     summary = "A GHC plugin to remove support for recursion";
-    file = {
-      "core/LICENSE".source = ../../LICENSE;
-      "core/LICENSE.AGPL-3.0-only".source = ../../LICENSE.AGPL-3.0-only;
-      "core/LICENSE.Universal-FOSS-exception-1.0".source =
-        ../../LICENSE.Universal-FOSS-exception-1.0;
-      "core/LICENSE.commercial".source = ../../LICENSE.commercial;
-    };
+    ## TODO: Move something like this to Flaky.
+    file = let
+      copyLicenses = dir: {
+        "${dir}/LICENSE".source = ../../LICENSE;
+        "${dir}/LICENSE.AGPL-3.0-only".source = ../../LICENSE.AGPL-3.0-only;
+        "${dir}/LICENSE.Universal-FOSS-exception-1.0".source =
+          ../../LICENSE.Universal-FOSS-exception-1.0;
+        "${dir}/LICENSE.commercial".source = ../../LICENSE.commercial;
+      };
+    in
+      copyLicenses "core";
   };
 
   imports = [./hlint.nix];
 
-  ## dependency management
-  services.renovate.enable = true;
-
-  ## development
-  programs = {
-    direnv.enable = true;
-    # This should default by whether there is a .git file/dir (and whether it’s
-    # a file (worktree) or dir determines other things – like where hooks
-    # are installed.
-    git.enable = true;
-  };
-
-  programs = {
-    treefmt.enable = true;
-    vale.enable = true;
-  };
-
-  ## formatting
-  editorconfig.enable = true;
-
   ## CI
-  services.garnix.enable = true;
   ## FIXME: Shouldn’t need `mkForce` here (or to duplicate the base contexts).
   ##        Need to improve module merging.
   services.github.settings.branches.main.protection.required_status_checks.contexts =
@@ -68,6 +50,5 @@
   };
 
   ## publishing
-  services.github.enable = true;
   services.github.settings.repository.topics = ["recursion" "plugin"];
 }
