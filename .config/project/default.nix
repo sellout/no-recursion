33bd3a4c50e9afbf485aa0105aff34747d808b39
Greg Pfeil <greg@technomadic.org>
2026-03-15 22:46:41 -0600

    Update `extraDependencyVersions` to new location

--- .config/project/default.nix
+++ .config/project/default.nix
@@ -66,7 +66,7 @@
     inherit (self.lib) defaultGhcVersion;
     ghcVersions = self.lib.nonNixTestedGhcVersions;
     cabalPackages = {"${config.project.name}" = "core";};
-    extraDependencyVersions = ["doctest-0.24.0"];
+    checkBounds.extraDependencyVersions = ["doctest-0.24.0"];
     latestGhcVersion = "9.10.1";
   };
 
